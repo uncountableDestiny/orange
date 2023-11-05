c37e181695283598fb255186416a055b26c0a251
daniele <daniele.mariotto@gmail.com>
2023-11-05 13:15:59 +0100

    1

---
+++ Python Input and Output Exercise/1.py
@@ -0,0 +1,8 @@
+"""
+1: Accept numbers from a user
+Write a program to accept two numbers from the user and calculate multiplication 
+"""
+print("Insert 2 numbers to multiplicate")
+a = input("First: ")
+b= input("Second: ")
+print("result = ", float(a) * float(b))
